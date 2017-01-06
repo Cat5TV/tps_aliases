b19ab851dc92a76bc199d246e89a5797588d89ba
Robbie Ferguson <contact@category5.tv>
2017-01-06 12:13:45 -0500

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,15 @@
+minetest.register_alias("path6alt:junglewood","default:junglewood")
+
+-- Removed DonBatman's old vines mod in favor of VanessaE's Plant Life mod
+-- Remove Don's vines, as they cause problems
+minetest.register_alias("vines:side_middle","air")
+minetest.register_alias("vines:side_end","air")
+minetest.register_alias("vines:root_end","air")
+minetest.register_alias("vines:root_middle","air")
+
+minetest.register_alias("vines:jungle_middle","air")
+minetest.register_alias("vines:jungle_end","air")
+minetest.register_alias("vines:vine_middle","air")
+minetest.register_alias("vines:vine_end","air")
+minetest.register_alias("vines:willow_middle","air")
+minetest.register_alias("vines:willow_end","air")

46d774393985a6d56f3dfc9f7442eb1e9cef1e92
tenplus1 <tenplus1@users.noreply.github.com>
2017-01-06 18:10:01 +0000

    add more alias'
    add more alias' and for loop for table containing them

--- init.lua
+++ init.lua
@@ -1,15 +1,52 @@
-minetest.register_alias("path6alt:junglewood","default:junglewood")
+local gold = "default:goldblock"
+local diamond = "default:diamondblock"
 
--- Removed DonBatman's old vines mod in favor of VanessaE's Plant Life mod
--- Remove Don's vines, as they cause problems
-minetest.register_alias("vines:side_middle","air")
-minetest.register_alias("vines:side_end","air")
-minetest.register_alias("vines:root_end","air")
-minetest.register_alias("vines:root_middle","air")
+local alias = {
+  {"path6alt:junglewood", "default:junglewood"},
 
-minetest.register_alias("vines:jungle_middle","air")
-minetest.register_alias("vines:jungle_end","air")
-minetest.register_alias("vines:vine_middle","air")
-minetest.register_alias("vines:vine_end","air")
-minetest.register_alias("vines:willow_middle","air")
-minetest.register_alias("vines:willow_end","air")
+-- remove vines
+  {"vines:side_middle", "air"},
+  {"vines:side_end", "air"},
+  {"vines:root_middle", "air"},
+  {"vines:root_end", "air"},
+  {"vines:jungle_middle", "air"},
+  {"vines:jungle_end", "air"},
+  {"vines:vine_middle", "air"},
+  {"vines:vine_end", "air"},
+  {"vines:willow_middle", "air"},
+  {"vines:willow_end", "air"},
+
+-- replace technic machines
+  {"technic:lv_electric_furnace", gold},
+  {"technic:lv_electric_furnace_active", gold},
+  {"technic:mv_electric_furnace", diamond},
+  {"technic:mv_electric_furnace_active", diamond},
+  {"technic:coal_alloy_furnace", gold},
+  {"technic:coal_alloy_furnace_active", gold},
+  {"technic:alloy_furnace", gold},
+  {"technic:alloy_furnace_active", gold},
+  {"technic:mv_alloy_furnace", diamond},
+  {"technic:mv_alloy_furnace_active", diamond},
+  {"technic:tool_workshop", gold},
+  {"technic:grinder", gold},
+  {"technic:grinder_active", gold},
+  {"technic:mv_grinder", diamond},
+  {"technic:mv_grinder_active", diamond},
+  {"technic:extractor", gold},
+  {"technic:extractor_active", gold},
+  {"technic:mv_extractor", diamond},
+  {"technic:mv_extractor_active", diamond},
+  {"technic:compressor", gold},
+  {"technic:compressor_active", gold},
+  {"technic:mv_compressor", diamond},
+  {"technic:mv_compressor_active", diamond},
+  {"technic:cnc", gold},
+  {"technic:cnc_active", gold},
+  {"technic:mv_centrifuge", diamond},
+  {"technic:mv_centrifuge_active", diamond},
+}
+
+-- change row[1] into row[2] on map
+for _, row in pairs(alias) do
+  minetest.register_alias(row[1], row[2])
+end
